79c3c6ee2c07c2e3a3a78e8204a5627b2d167c80
Denis Tupichkin <denis.tupichkin@white-soft.ru>
2016-11-21 11:51:27 +1000

    fix bug

--- src/main/java/com/whitesoft/messaging/services/FCMessagingSerivce.java
+++ src/main/java/com/whitesoft/messaging/services/FCMessagingSerivce.java
@@ -1,11 +1,8 @@
 package com.whitesoft.messaging.services;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.JsonObject;
-import com.sun.javafx.binding.StringFormatter;
 import com.whitesoft.announce.model.Announce;
 import com.whitesoft.messaging.util.FCMHelper;
-import com.whitesoft.messaging.util.JsonUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -21,15 +18,19 @@ public class FCMessagingSerivce implements MessagingService {
     public void sendNotificationAboutNewAds(Announce ads) {
 
         JsonObject payLoad = new JsonObject();
-        payLoad.addProperty("id", ads.getId().toString() );
         payLoad.addProperty("title",ads.getHeader());
         payLoad.addProperty("body", ads.getText());
 
-        logger.info(StringFormatter.format("Notification: {%s}", payLoad.toString()).getValue());
+        JsonObject data = new JsonObject();
+        data.addProperty("id", ads.getId().toString() );
+
+        logger.info(String.format("Notification: {%s}", payLoad.toString()));
 
         try{
             FCMHelper fcmHelper = FCMHelper.getInstance();
-            fcmHelper.sendNotification(TYPE_TO,"/topics/ads", payLoad);
+//            fcmHelper.sendNotifictaionAndData(TYPE_TO, "topics/ads", payLoad, data);
+//            fcmHelper.sendTopicNotification("ads", payLoad);
+            fcmHelper.sendTopicNotificationAndData("ads", payLoad, data);
         }catch (Exception ex)
         {
             logger.error(ex.getLocalizedMessage());
